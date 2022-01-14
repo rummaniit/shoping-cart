9e8c85c9a4f68f4107b7336468498cbc6d81fe78
rummaniit <rummanoffice12@gmail.com>
2022-01-14 11:59:16 +0600

    added

---
+++ scipt.js
@@ -0,0 +1,8 @@
+let decrease = document.getElementById('plus')
+decrease.addEventListener('click', function () {
+    let value = document.getElementById('val').innerText
+    let sum = 0
+    let valuefloat = parseFloat(value)
+    valuefloat += sum
+
+})
